20f837aa0c20f98e17f8e8b052269647a42daf48
mallaoui <maher.gallaoui@robinson.com>
2025-12-06 01:58:19 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  pageExtensions: ["ts", "tsx"],
+  turbopack: {
+    root: __dirname,
+  },
 };
-
 export default nextConfig;
